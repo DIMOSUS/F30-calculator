e92e2b879f5564af2fc11d5d872dbb5f601ab914
dimosus.x <dimosus.x@gmail.com>
2020-04-03 14:29:17 +0300

    DSC

--- AccelerationCalculator/Program.cs
+++ AccelerationCalculator/Program.cs
@@ -14,35 +14,54 @@ namespace AccelerationCalculator
 
         //f30
         static readonly double EnginePower = 280 * w2hp;
-        static readonly double Mass = 1620;//(With Wells)
+        static readonly double Mass = 1630;//(With Wells)
         static readonly double WellsMass = 100;//Included in the mass of cars
         static readonly double FrontArea = 2.2;
         static readonly double TransmissionEfficiency = 0.82;
-        static readonly double TurboLagTime = 0.4;//2.2 for n20 without launch
+        static readonly double TurboLagTime = 0.0;//2.2 for n20 without launch
 
 
         //zf8 45hp
         static readonly double[] GearRatios = new double[] { 4.714, 3.143, 2.106, 1.667, 1.285, 1.000, 0.839, 0.667 };
         static readonly double FirstGearShift = 6500;
-        static readonly double OtherGearShift = 6800;
-        static readonly double GearShiftTime = 0.15;
-        static readonly double FirstGearMaxSpeed = 55 * kmh2ms;//kmh
+        static readonly double OtherGearShift = 6700;
+        static readonly double LaunchEngSpeed = 2500;
+        static readonly double GearShiftTime = 0.2;
+        static readonly double FirstGearMaxSpeed = 52 * kmh2ms;//kmh
+        static readonly double DSCReaction = 1500;//eng speed per sec
+        static readonly double GearСlutchStart = 0.4;
 
-        static readonly double Friction = 1.08;
+        static readonly double Friction = 1.0;
 
         //BMW n20
+        static readonly (double, double)[] EngPower = new (double, double)[] {
+            (0, 0),
+            (700, 0),
+            (1250, EnginePower/4),
+            (5000, EnginePower),
+            (6500, EnginePower),
+            (7000, EnginePower*0.94)
+        };
+
         static double GetEnginePower(double EngineSpeed)
         {
-            if (EngineSpeed < 1250)
-                return EngineSpeed / 1250 * EnginePower * 0.25;
-            if (EngineSpeed < 5000)
-                return EngineSpeed / 5000 * EnginePower;
-            return EnginePower;
+            if (EngineSpeed >= EngPower[^1].Item1)
+                return EngPower[^1].Item2;
+
+            for (int i = EngPower.Length - 1; i > -1; i--)
+            {
+                if(EngineSpeed > EngPower[i].Item1)
+                {
+                    double lval = (EngineSpeed - EngPower[i].Item1) / (EngPower[i + 1].Item1 - EngPower[i].Item1);
+                    return lerp(EngPower[i].Item2, EngPower[i + 1].Item2, lval);
+                }
+            }
+            return 0;
         }
 
         static double GetEngineSpeed(int Gear, double Speed)
         {
-            return Math.Max(FirstGearShift / FirstGearMaxSpeed * Speed * GearRatios[Gear - 1] / GearRatios[0], 1500);
+            return Math.Max(FirstGearShift / FirstGearMaxSpeed * Speed * GearRatios[Gear - 1] / GearRatios[0], 900);
         }
 
         static double AirResistance(double speed)
@@ -54,20 +73,19 @@ namespace AccelerationCalculator
             return Cx * FrontArea * p * x * x * x / 2;
         }
 
-        static double GetWellPower(double Speed, double Acceleration_g, double EngineSpeed, double turboLagTime, ref double WheelSlipTime)
+        static double GetWellPower(double Speed, double Acceleration_g, double EngineSpeed, double turboLagTime, ref bool WheelSlip)
         {
             double Fk = 0.01 * (1 + 5.5E-4 * Speed * Speed);
 
             double dirtPower = GetEnginePower(EngineSpeed) * TransmissionEfficiency * (turboLagTime > 0 ? 0.5 : 1.0);
             dirtPower *= 1 - Fk;
-            dirtPower -= AirResistance(Speed);
 
             //P=F*V
             double dirtForce = dirtPower / (Speed + 0.1);
             double theoreticalForceLimitAWD = Mass * 9.8 * Friction;// * ((RWD ? 0.75 : 0) + (FWD ? 0.25 : 0));
             if (RWD && !FWD)
             {
-                double rw_ballance = 0.28 * Acceleration_g;
+                double rw_ballance = 0.35 * Acceleration_g;
                 theoreticalForceLimitAWD *= 0.5 + rw_ballance;
             }
             if (!RWD && FWD)
@@ -76,11 +94,18 @@ namespace AccelerationCalculator
                 theoreticalForceLimitAWD *= 0.5 - fw_ballance;
             }
 
-            if (dirtForce > theoreticalForceLimitAWD)
+
+            double slipK = Math.Min((dirtForce / theoreticalForceLimitAWD - 1) / 0.3, 1);
+
+            if (slipK > 0)
             {
-                WheelSlipTime += TickSize;
-                return dirtPower * (theoreticalForceLimitAWD / dirtForce) * 0.85;
+                WheelSlip = true;
+                return dirtPower * (theoreticalForceLimitAWD / dirtForce) * lerp(1, 0.8, slipK);
             }
+            else
+                WheelSlip = false;
+
+            dirtPower -= AirResistance(Speed);
 
             return dirtPower;
         }
@@ -91,6 +116,7 @@ namespace AccelerationCalculator
             Graph g_e_speed = new Graph("e_speed", 500, 300, 20, 7000, 1, 1000 );
             Graph g_weel_pow = new Graph("weel_pow", 500, 300, 20, EnginePower / w2hp, 1, 50 );
             Graph g_acc = new Graph("acceleration", 500, 300, 20, 2, 1, 1 );
+            Graph g_e_pwr = new Graph("e_pwr", 500, 300, 7000, EnginePower / w2hp * 1.1, 1000, 50 );
 
             double[] times = new double[30];
 
@@ -103,10 +129,18 @@ namespace AccelerationCalculator
             double quarterSpeed = 0;
             double turboLagTime = TurboLagTime;
             double acceleration_g = 0;
+            double gearСlutch = GearСlutchStart;
+            double dscCorr = 0;
             int quarterGear = 0;
+            bool wheelSlip = false;
 
             bool needGearShift = false;
             int gear = 1;
+
+            for (int i = 0; i < 7000; i++)
+            {
+                g_e_pwr.Point(i, GetEnginePower(i) / w2hp);
+            }
 
             Console.WriteLine("km/h\ttime");
 
@@ -146,12 +180,23 @@ namespace AccelerationCalculator
                     continue;
                 }
 
-
                 engineSpeed = GetEngineSpeed(gear, speed);
+
+                //gearСlutch
+                if (gearСlutch > 0)
+                    engineSpeed = lerp(engineSpeed, LaunchEngSpeed, smoothstep(gearСlutch / GearСlutchStart));
+                gearСlutch -= TickSize;
+
+                //DSC
+                if (wheelSlip)
+                    dscCorr += DSCReaction * TickSize;
+                else
+                    dscCorr = Math.Max(0, dscCorr - DSCReaction * TickSize * 3);
+                engineSpeed -= dscCorr;
 
                 double prePower = (Mass + WellsMass * 0.8) * speed * speed / 2;
 
-                double powerInc = GetWellPower(speed, acceleration_g, engineSpeed, turboLagTime, ref wheelSlipTime);
+                double powerInc = GetWellPower(speed, acceleration_g, engineSpeed, turboLagTime, ref wheelSlip);
 
                 double new_speed = Math.Sqrt(2 * (prePower + powerInc * TickSize) / (Mass + WellsMass * 0.8));
 
@@ -159,6 +204,7 @@ namespace AccelerationCalculator
 
                 speed = new_speed;
 
+                if (wheelSlip) wheelSlipTime += TickSize;
                 time += TickSize;
                 turboLagTime -= TickSize;
                 distance += speed * TickSize;
@@ -179,6 +225,7 @@ namespace AccelerationCalculator
                         needGearShift = true;
                 }
             }
+
             Console.WriteLine();
             Console.WriteLine("Quarter \t{0}", Math.Round(quarter, 2));
             Console.WriteLine("Quarter Speed \t{0}", Math.Round(quarterSpeed, 2));
@@ -190,6 +237,29 @@ namespace AccelerationCalculator
             g_e_speed.Save();
             g_weel_pow.Save();
             g_acc.Save();
+            g_e_pwr.Save();
+        }
+
+        static double lerp(double v0, double v1, double t)
+        {
+            return (1 - t) * v0 + t * v1;
+        }
+
+        static double smoothstep(double x, double edge0 = 0, double edge1 = 1)
+        {
+            // Scale, bias and saturate x to 0..1 range
+            x = clamp((x - edge0) / (edge1 - edge0), 0.0, 1.0);
+            // Evaluate polynomial
+            return x * x * (3 - 2 * x);
+        }
+
+        static double clamp(double x, double lowerlimit, double upperlimit)
+        {
+            if (x < lowerlimit)
+                x = lowerlimit;
+            if (x > upperlimit)
+                x = upperlimit;
+            return x;
         }
     }
 }
